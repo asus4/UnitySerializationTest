8326b44598658289bc8c4ef0faa0de811300c064
Koki Ibukuro <koki.ibukuro@gmail.com>
2023-02-25 00:23:34 +0100

    Add more tests

--- Assets/Tests/PerformanceTest.cs
+++ Assets/Tests/PerformanceTest.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 using UnityEngine;
 using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
+using Unity.Mathematics;
 using Unity.PerformanceTesting;
 using Unity.Properties;
 using Unity.Serialization.Binary;
@@ -53,7 +54,7 @@ public class PerformanceTest
 
 [Serializable]
 [GeneratePropertyBag]
-public class TestData : IEquatable<TestData>
+public partial class TestData : IEquatable<TestData>
 {
     public enum Mode
     {
@@ -64,10 +65,14 @@ public class TestData : IEquatable<TestData>
     public int integer = 10;
     public float floating = 1.5f;
     public Vector3 vector = new(1, 2, 3);
+    public Quaternion quaternion = new(1, 0, 0, 1);
     public Color color = new(1, 0, 0, 1);
     public Mode mode = Mode.A;
     public byte[] bytes = new byte[1000];
-
+    public Pose pose = new(new Vector3(1, 2, 3), new Quaternion(1, 0, 0, 1));
+    public float4x4 floatMatrix = new(new float4(1, 0, 0, 0), new float4(0, 1, 0, 0), new float4(0, 0, 1, 0), new float4(0, 0, 0, 1));
+    public Matrix4x4 matrix = new(new Vector4(1, 0, 0, 0), new Vector4(0, 1, 0, 0), new Vector4(0, 0, 1, 0), new Vector4(0, 0, 0, 1));
+    public InnerClass innerClass = new();
 
     private static readonly UnsafeAppendBuffer s_Stream = new(16, 8, Allocator.Persistent);
 
@@ -82,7 +87,11 @@ public class TestData : IEquatable<TestData>
             && vector == other.vector
             && color == other.color
             && mode == other.mode
-            && bytes.Length == other.bytes.Length;
+            && bytes.Length == other.bytes.Length
+            && pose == other.pose
+            && floatMatrix.Equals(other.floatMatrix)
+            && matrix == other.matrix
+            && innerClass.Equals(other.innerClass);
     }
 
     public unsafe NativeArray<byte> Serialize(Allocator allocator)
@@ -110,5 +119,21 @@ public class TestData : IEquatable<TestData>
     public override string ToString()
     {
         return $"TestData {{ text: {text}, integer: {integer}, floating: {floating}, vector: {vector}, color: {color}, mode: {mode}, bytes: {bytes.Length} bytes }}";
+    }
+}
+
+public partial class TestData
+{
+    [Serializable]
+    [GeneratePropertyBag]
+    public class InnerClass: IEquatable<InnerClass>
+    {
+        public string text = "inner class text";
+        public bool Equals(InnerClass other)
+        {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return text == other.text;
+        }
     }
 }
